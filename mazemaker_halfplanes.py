a3f33ca83a42a997158f96ebc3d2c94e109e888f
Brian Bock <bbock@terpmail.umd.edu>
2020-03-05 11:27:14 -0500

    improved code readability and consistency

--- mazemaker_halfplanes.py
+++ mazemaker_halfplanes.py
@@ -69,10 +69,13 @@ def mazeMaker(mazetype): #mazetype can be either "Trial" or "Final"
 		ellipse_centery=100
 		ellipse_major=40
 		ellipse_minor=20
-		for x in range (circle_boundbox_x,circle_boundbox_x+2*radius):
-			for y in range(circle_boundbox_y,circle_boundbox_y+2*radius):
+		ellipse_boundbox_x=110
+		ellipse_boundbox_y=80
+		for x in range (ellipse_boundbox_x,circle_boundbox_x+2*ellipse_major):
+			for y in range(ellipse_boundbox_y,ellipse_boundbox_y+2*ellipse_minor):
 				if(((x-ellipse_centerx)**2/ellipse_major**2)+((y-ellipse_centery)**2/ellipse_minor**2)<=1):
-						finalmaze[y][x]=(255,255,255)
+					# print("making ellipse")
+					finalmaze[y][x]=(255,255,255)
 
 
 
@@ -85,21 +88,26 @@ def mazeMaker(mazetype): #mazetype can be either "Trial" or "Final"
 		diamond_slopes.append(getSlope(diamondpts[2][0],diamondpts[2][1],diamondpts[3][0],diamondpts[3][1]))
 		diamond_slopes.append(getSlope(diamondpts[3][0],diamondpts[3][1],diamondpts[0][0],diamondpts[0][1]))
 
-		diamond_b1=getIntercept(diamondpts[1][0],diamondpts[1][1],diamondpts[2][0],diamondpts[2][1])
-		diamond_b2=getIntercept(diamondpts[3][0],diamondpts[3][1],diamondpts[0][0],diamondpts[0][1])
-		diamond_b3=getIntercept(diamondpts[2][0],diamondpts[2][1],diamondpts[3][0],diamondpts[3][1])
-		diamond_b4=getIntercept(diamondpts[0][0],diamondpts[0][1],diamondpts[1][0],diamondpts[1][1])
+		diamond_b=[]
+		diamond_b.append(getIntercept(diamondpts[0][0],diamondpts[0][1],diamondpts[1][0],diamondpts[1][1]))
+		diamond_b.append(getIntercept(diamondpts[1][0],diamondpts[1][1],diamondpts[2][0],diamondpts[2][1]))
+		diamond_b.append(getIntercept(diamondpts[2][0],diamondpts[2][1],diamondpts[3][0],diamondpts[3][1]))
+		diamond_b.append(getIntercept(diamondpts[3][0],diamondpts[3][1],diamondpts[0][0],diamondpts[0][1]))
+
+		# print("Done with diamond slopes and intercepts")
+		# print(diamond_b1)
 		# cv2.drawContours(finalmaze,[diamondpts],-1,(255,255,255),-1)
 
-		# Half Planes
-		for x in range(225, 250):
-			for y in range(160,190):
-				y1=diamond_slopes[1]*x+diamond_b1
-				y2=diamond_slopes[3]*x+diamond_b2
-				y3=diamond_slopes[2]*x+diamond_b3
-				y4=diamond_slopes[0]*x+diamond_b4
-
-				if y<y1 and y<y3 and y>y4 and y>y2:
+		# Half Planes for Diamond
+		for x in range(225, 251):
+			for y in range(160,191):
+				y1=diamond_slopes[2]*x+diamond_b[2]
+				y2=diamond_slopes[0]*x+diamond_b[0]
+				y3=diamond_slopes[1]*x+diamond_b[1]
+				y4=diamond_slopes[3]*x+diamond_b[3]
+				print(y1,y2,y3,y4)
+				if (y<y1) and (y<y3) and (y>y4) and (y>y2):
+					print("making diamond")
 					finalmaze[y][x]=(255,255,255)
 
 
@@ -119,18 +127,25 @@ def mazeMaker(mazetype): #mazetype can be either "Trial" or "Final"
 		rect_slopes.append(getSlope(x3,y3,x4,y4))
 		rect_slopes.append(getSlope(x4,y4,x1,y1))
 
-		rect_b1=getIntercept(x2,y2,x3,y3)
-		rect_b2=getIntercept(x4,y4,x1,y1)
-		rect_b3=getIntercept(x3,y3,x4,y4)
-		rect_b4=getIntercept(x4,y4,x1,y1)
+		rect_b=[]
+		rect_b.append(getIntercept(x1,y1,x2,y2))
+		rect_b.append(getIntercept(x2,y2,x3,y3))
+		rect_b.append(getIntercept(x3,y3,x4,y4))
+		rect_b.append(getIntercept(x4,y4,x1,y1))
+
+
+		# 	=getIntercept(x2,y2,x3,y3)
+		# rect_b2=getIntercept(x4,y4,x1,y1)
+		# rect_b3=getIntercept(x3,y3,x4,y4)
+		# rect_b4=getIntercept(x4,y4,x1,y1)
 
 		# Half Planes
 		for x in range(int(x2), int(x4)):
 			for y in range(int(y3),int(y1)):
-				y1=rect_slopes[1]*x+rect_b1
-				y2=rect_slopes[3]*x+rect_b2
-				y3=rect_slopes[2]*x+rect_b3
-				y4=rect_slopes[0]*x+rect_b4
+				y1=rect_slopes[1]*x+rect_b[1]
+				y2=rect_slopes[3]*x+rect_b[3]
+				y3=rect_slopes[2]*x+rect_b[2]
+				y4=rect_slopes[0]*x+rect_b[0]
 
 				if y<y1 and y<y3 and y>y4 and y>y2:
 					finalmaze[y][x]=(255,255,255)
@@ -148,19 +163,20 @@ def mazeMaker(mazetype): #mazetype can be either "Trial" or "Final"
 		poly_slopes1.append(getSlope(polypts[4][0],polypts[4][1],polypts[5][0],polypts[5][1]))
 		poly_slopes1.append(getSlope(polypts[5][0],polypts[5][1],polypts[0][0],polypts[0][1]))
 
-		poly1_b1=getIntercept(polypts[1][0],polypts[1][1],polypts[4][0],polypts[4][1])
-		poly1_b2=getIntercept(polypts[5][0],polypts[5][1],polypts[0][0],polypts[0][1])
-		poly1_b3=getIntercept(polypts[4][0],polypts[4][1],polypts[5][0],polypts[5][1])
-		poly1_b4=getIntercept(polypts[0][0],polypts[0][1],polypts[1][0],polypts[1][1])
+		poly1_b=[]
+		poly1_b.append(getIntercept(polypts[0][0],polypts[0][1],polypts[1][0],polypts[1][1]))
+		poly1_b.append(getIntercept(polypts[1][0],polypts[1][1],polypts[4][0],polypts[4][1]))
+		poly1_b.append(getIntercept(polypts[4][0],polypts[4][1],polypts[5][0],polypts[5][1]))
+		poly1_b.append(getIntercept(polypts[5][0],polypts[5][1],polypts[0][0],polypts[0][1]))
 
 
 		# Half Planes for Left Half of polygon
 		for x in range(20, 75):
 			for y in range(15,80):
-				y1=poly_slopes1[1]*x+poly1_b1
-				y2=poly_slopes1[3]*x+poly1_b2
-				y3=poly_slopes1[2]*x+poly1_b3
-				y4=poly_slopes1[0]*x+poly1_b4
+				y1=poly_slopes1[1]*x+poly1_b[1]
+				y2=poly_slopes1[3]*x+poly1_b[3]
+				y3=poly_slopes1[2]*x+poly1_b[2]
+				y4=poly_slopes1[0]*x+poly1_b[0]
 
 				if y<y1 and y<y3 and y>y4 and y>y2:
 					finalmaze[y][x]=(255,255,255)
@@ -172,27 +188,28 @@ def mazeMaker(mazetype): #mazetype can be either "Trial" or "Final"
 		poly_slopes2.append(getSlope(polypts[3][0],polypts[3][1],polypts[4][0],polypts[4][1]))
 		poly_slopes2.append(getSlope(polypts[4][0],polypts[4][1],polypts[1][0],polypts[1][1]))
 
-		poly2_b1=getIntercept(polypts[4][0],polypts[4][1],polypts[1][0],polypts[1][1])
-		poly2_b2=getIntercept(polypts[2][0],polypts[2][1],polypts[3][0],polypts[3][1])
-		poly2_b3=getIntercept(polypts[1][0],polypts[1][1],polypts[2][0],polypts[2][1])
-		poly2_b4=getIntercept(polypts[3][0],polypts[3][1],polypts[4][0],polypts[4][1])
+		poly2_b=[]
+		poly2_b.append(getIntercept(polypts[1][0],polypts[1][1],polypts[2][0],polypts[2][1]))
+		poly2_b.append(getIntercept(polypts[2][0],polypts[2][1],polypts[3][0],polypts[3][1]))
+		poly2_b.append(getIntercept(polypts[3][0],polypts[3][1],polypts[4][0],polypts[4][1]))
+		poly2_b.append(getIntercept(polypts[4][0],polypts[4][1],polypts[1][0],polypts[1][1]))
 
 
 		# Half Planes for Right Half of polygon
 		for x in range(50, 100):
 			for y in range(15,80):
-				y1=poly_slopes2[3]*x+poly2_b1
-				y2=poly_slopes2[1]*x+poly2_b2
-				y3=poly_slopes2[0]*x+poly2_b3
-				y4=poly_slopes2[2]*x+poly2_b4
+				y1=poly_slopes2[3]*x+poly2_b[3]
+				y2=poly_slopes2[1]*x+poly2_b[1]
+				y3=poly_slopes2[0]*x+poly2_b[0]
+				y4=poly_slopes2[2]*x+poly2_b[2]
 
 				if y<y1 and y<y3 and y>y4 and y>y2:
 					finalmaze[y][x]=(255,255,255)
 
 
 		# cv2.drawContours(finalmaze,[polypts],-1,(255,255,255),-1)
-		#cv2.imshow("The maze",finalmaze)
-		#cv2.waitKey(0)
+		cv2.imshow("The maze",finalmaze)
+		cv2.waitKey(0)
         
         
 		maze=np.empty((height,width),dtype='object')
@@ -212,6 +229,6 @@ def mazeMaker(mazetype): #mazetype can be either "Trial" or "Final"
 		return maze
 
 
-# mazeMaker("final")
+mazeMaker("final")
 
 
